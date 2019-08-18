1fd4d91544e104708c5133357812cf163ed6dc27
I326319 <ranganath.sn@gmail.com>
2019-08-18 13:35:23 +0530

    annotation configuration using xml

--- src/main/java/com/snr/service/UserServiceImpl.java
+++ src/main/java/com/snr/service/UserServiceImpl.java
@@ -5,6 +5,9 @@ package com.snr.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.snr.model.User;
 import com.snr.repository.UserRepository;
 
@@ -12,15 +15,19 @@ import com.snr.repository.UserRepository;
  * @author I326319
  *
  */
+@Service("userService")
 public class UserServiceImpl implements UserService {
 	
+	//@Autowired
 	private UserRepository userRepository;
 	
 	public UserServiceImpl() {
 		
 	}
 	
+	@Autowired
 	public UserServiceImpl(UserRepository userRepository) {
+		System.out.println("we are using autowired in constructor");
 		this.userRepository=userRepository;
 	}
 	
@@ -32,8 +39,9 @@ public class UserServiceImpl implements UserService {
 		return userRepository.findAll();
 	}
 
-
+	//@Autowired
 	public void setUserRepository(UserRepository userRepository) {
+		//System.out.println("we are using autowired in setter");
 		this.userRepository = userRepository;
 	}
 
